63cb15fa4e65024d6a8850999a3008251a4ec2a9
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-08 11:04:29 +0200

    Debug print head ID

--- app/controllers/inbox.js
+++ app/controllers/inbox.js
@@ -2,8 +2,11 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import sendMail from '../utils/send-mail';
 import toggleReadStatus from '../utils/toggle-mail-read-status';
+import { inject as service } from '@ember/service';
 
 export default class InboxController extends Controller {
+  @service headIdentification;
+
   @action
   sendMail () {
     sendMail();
